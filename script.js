2f508db0126061a3a11ceb465e286f141c473c74
Zdenek Andrew Bocek <zabocek@gmail.com>
2023-06-03 13:02:57 -0400

    I think I've completed unit 16

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-const input = document.querySelector('#fruit');
-const suggestions = document.querySelector('.suggestions ul');
+const inputBox = document.getElementById('fruit');
+const suggestions = document.querySelector('.suggestions');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
-function search(str) {
-	let results = [];
-
-	// TODO
-
-	return results;
+input.onkeyup = function(){
+	let result = [];
+	let input = inputBox.value;
+	if(input.length){
+		result = fruit.filter((keyword)=>{
+			return keyword.toLowerCase().includes(input.toLowerCase());
+		});
+		console.log(result);
+	}
+	display(result);
+	if(!result.length){
+		suggestions.innerHTML = '';
+	}
 }
 
-function searchHandler(e) {
-	// TODO
+function display(result){
+	const content = result.map((list)=>{
+		return "<li onclick=selectInput(this)>" + list + "</li>";
+	});
+	suggestions.innerHTML = "<ul>" + content.join('') + "</ul>";
 }
 
-function showSuggestions(results, inputVal) {
-
-	// TODO
+function selectInput(list){
+	inputBox.value = list.innerHTML;
+	suggestions.innerHTML = '';
 }
-
-function useSuggestion(e) {
-	// TODO
-}
-
-input.addEventListener('keyup', searchHandler);
-suggestions.addEventListener('click', useSuggestion);
