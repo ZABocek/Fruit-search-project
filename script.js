0a25b44c5e574bbefe4d861e2dfb4249ea6ac778
Zdenek Andrew Bocek <zabocek@gmail.com>
2023-06-03 14:25:14 -0400

    Unit 16 possibly complete

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const inputBox = document.querySelector('.fruit-box');
-const suggestions = document.querySelector('.suggestions');
-
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
-input.onkeyup = function(){
+const resultsBox = document.querySelector('.result-box');
+const inputBox = document.getElementById('input-box');
+
+inputBox.onkeyup = function(){
 	let result = [];
 	let input = inputBox.value;
 	if(input.length){
@@ -14,7 +14,7 @@ input.onkeyup = function(){
 	}
 	display(result);
 	if(!result.length){
-		suggestions.innerHTML = '';
+		resultsBox.innerHTML = '';
 	}
 }
 
@@ -22,10 +22,10 @@ function display(result){
 	const content = result.map((list)=>{
 		return "<li onclick=selectInput(this)>" + list + "</li>";
 	});
-	suggestions.innerHTML = "<ul>" + content.join('') + "</ul>";
+	resultsBox.innerHTML = "<ul>" + content.join('') + "</ul>";
 }
 
 function selectInput(list){
 	inputBox.value = list.innerHTML;
-	suggestions.innerHTML = '';
+	resultsBox.innerHTML = '';
 }
